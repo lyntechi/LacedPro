4b733243559416a717e0da0597bdbdabbb82cca3
Lynda Santiago <lyntechi@gmail.com>
2020-12-08 00:02:09 -0500

    switched from video to gif

--- app/src/components/Homepage.jsx
+++ app/src/components/Homepage.jsx
@@ -4,24 +4,19 @@ import { Link } from "react-router-dom";
 export default function Homepage() {
   return (
     <div className="homePage">
-      <video autoPlay={true} loop={true}>
-        <source
-          src="https://static.nike.com/a/videos/q_90,vc_vp9/mccsot59vf09htkvgzdm/video.webm"
-        />
-      </video>
       <div className="banner-text">
-      <p style={{fontStyle: 'italic'}}>
-         Designs that you will love
+        <p style={{ fontStyle: "italic" }}>
+          Designs that you will love
           <br />
         </p>
         <h2>
           Laced <span>Pro</span>
           <br />
         </h2>
-        
+
         <p>Sneakers for Women, Men & Kids</p>
         <br />
-        
+
         <Link to="/shop">
           <button>Shop now!</button>
         </Link>
