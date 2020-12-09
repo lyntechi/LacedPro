6876c65a38a278b167193af65c05986933bd78f7
Lynda Santiago <lyntechi@gmail.com>
2020-12-08 20:46:36 -0500

    sizing on images

--- app/src/components/MediaModal.jsx
+++ app/src/components/MediaModal.jsx
@@ -55,7 +55,7 @@ export default function MediaModal({
     <div style={{ display: "flex", flexDirection: "column" }}>
       <img src={product.gif} className="video" alt="" />
 
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <div style={{ display: "flex"}}>
         <img
           src={imageOne}
           className="smallImage"
@@ -68,6 +68,8 @@ export default function MediaModal({
           onClick={secondImage}
           alt="small sneaker icon"
         />
+        </div>
+        <div  style={{ display: "flex"}}>
         <img
           src={imageThree}
           className="smallImage"
